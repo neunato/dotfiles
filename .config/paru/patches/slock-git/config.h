012cf61c850121b46688f904b2a8622289262b76
Jan Ivica <wesleymustdie@gmail.com>
2024-06-27 21:27:05 +0200

    replace i3lock with slock

---
+++ .config/paru/patches/slock-git/config.h
@@ -0,0 +1,8 @@
+static const char *user  = "nobody";
+static const char *group = "nobody";
+static const char *colorname[NUMCOLS] = {
+   [INIT] =   "#161c26",
+   [INPUT] =  "#0f131a",
+   [FAILED] = "#161c26",
+};
+static const int failonclear = 1;
